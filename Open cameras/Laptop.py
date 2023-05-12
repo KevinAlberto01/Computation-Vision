bf2c8d3385b1bd552173d407065c235667548df2
Kevin Alberto <132956095+KevinAlberto01@users.noreply.github.com>
2023-05-11 22:28:21 -0600

    Update Laptop.py

--- Open cameras/Laptop.py
+++ Open cameras/Laptop.py
@@ -1,1 +1,14 @@
+import cv2 # import the library de Open vc2 
+ 
+cam = cv2.VideoCapture(0) # save in a variable the video camera 
 
+while True: # While the video camera is activate
+    ret, frame = cam.read() # Display the resulting frame
+    cv2.imshow('webCam',frame) # Show the value of the variable 
+    if (cv2.waitKey(1) == ord('s')): # set the exit buttom 
+        break
+
+cam.release() # Release the cap object
+cv2.destroyAllWindows() # Destroy all windows 
+
+  
